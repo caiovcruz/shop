42ca76a0ae875483d39c7bc0eb341c251760af6f
Caio Cruz <covu@gft.com>
2022-09-15 18:27:51 -0300

    Working on favorites/all filter feature for products overview page

--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,15 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../components/product_grid.dart';
+import '../models/product_list.dart';
+
+enum FilterOptions {
+  favorites,
+  all,
+}
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final provider = Provider.of<ProductList>(context);
+
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Store'),
+        actions: [
+          PopupMenuButton(
+            icon: const Icon(Icons.filter_alt),
+            itemBuilder: (_) => [
+              const PopupMenuItem(
+                value: FilterOptions.favorites,
+                child: Text('Only favorites'),
+              ),
+              const PopupMenuItem(
+                value: FilterOptions.all,
+                child: Text('All'),
+              ),
+            ],
+            onSelected: (FilterOptions selectedFilter) {
+              selectedFilter == FilterOptions.favorites
+                  ? provider.showFavoritesOnly()
+                  : provider.showAll();
+            },
+          )
+        ],
       ),
       body: const ProductGrid(),
     );
