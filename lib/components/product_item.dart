d27bdab6d2df99f5d57dd32ee0fcf6da18a298d8
Caio Cruz <covu@gft.com>
2022-09-15 18:11:08 -0300

    Refactoring to extract a better perfomance and clean code

--- lib/components/product_item.dart
+++ lib/components/product_item.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 import '../models/product.dart';
 import '../utils/app_routes.dart';
 
 class ProductItem extends StatelessWidget {
-  final Product product;
-
-  const ProductItem({
-    Key? key,
-    required this.product,
-  }) : super(key: key);
+  const ProductItem({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
+    final product = Provider.of<Product>(context, listen: false);
+
     return ClipRRect(
       borderRadius: BorderRadius.circular(10),
       child: GridTile(
         footer: GridTileBar(
           backgroundColor:
               Theme.of(context).colorScheme.tertiary.withOpacity(0.8),
-          leading: IconButton(
-            onPressed: () {},
-            icon: const Icon(Icons.favorite),
-            color: Theme.of(context).colorScheme.secondary,
-          ),
+          leading: Consumer<Product>(builder: (cxt, product, _) {
+            return IconButton(
+              onPressed: () => product.toggleFavorite(),
+              icon: Icon(
+                  product.isFavorite ? Icons.favorite : Icons.favorite_border),
+              color: Theme.of(context).colorScheme.secondary,
+            );
+          }),
           title: Text(
             product.title,
             textAlign: TextAlign.center,
--- lib/pages/products_overview_page.dart
+++ lib/pages/products_overview_page.dart
@@ -1,35 +1,17 @@
 import 'package:flutter/material.dart';
-import 'package:provider/provider.dart';
 
-import '../components/product_item.dart';
-import '../data/dummy_data.dart';
-import '../models/product.dart';
-import '../models/product_list.dart';
+import '../components/product_grid.dart';
 
 class ProductsOverviewPage extends StatelessWidget {
   const ProductsOverviewPage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final provider = Provider.of<ProductList>(context);
-    final List<Product> loadedProducts = provider.items;
-    
     return Scaffold(
       appBar: AppBar(
         title: const Text('My Store'),
       ),
-      body: GridView.builder(
-        padding: const EdgeInsets.all(10),
-        itemCount: loadedProducts.length,
-        gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-          crossAxisCount: 2,
-          childAspectRatio: 3 / 2,
-          crossAxisSpacing: 10,
-          mainAxisSpacing: 10,
-        ),
-        itemBuilder: (context, index) =>
-            ProductItem(product: loadedProducts[index]),
-      ),
+      body: const ProductGrid(),
     );
   }
 }
